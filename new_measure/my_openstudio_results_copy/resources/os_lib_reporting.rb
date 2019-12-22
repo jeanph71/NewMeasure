b8c2967d88b06c0a980fe7f96309dd9368d61a9a
costes <jean-philippe.costes@ensam.eu>
2019-12-22 19:29:38 +0100

    unités changées ligne 718

--- new_measure/my_openstudio_results_copy/resources/os_lib_reporting.rb
+++ new_measure/my_openstudio_results_copy/resources/os_lib_reporting.rb
@@ -715,7 +715,7 @@ module OsLib_Reporting
 
       # low speed
       sizing_source_units = 'W'
-      sizing_target_units = 'Btu/h'
+      sizing_target_units = 'W'
       if component.ratedLowSpeedTotalCoolingCapacity.is_initialized
         sizing_ip = OpenStudio.convert(component.ratedLowSpeedTotalCoolingCapacity.get, sizing_source_units, sizing_target_units).get
         sizing_ip_neat = OpenStudio.toNeatString(sizing_ip, 2, true)
